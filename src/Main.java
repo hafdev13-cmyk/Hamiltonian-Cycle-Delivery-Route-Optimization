63f9869fee852e8da2a5334c595aaec6425668d0
Haftom Ghiwet <hafdev13@gmail.com>
2026-04-29 05:32:57 -0700

    Refactor main method and remove GUI code
    Refactor main method to improve readability and remove unused code.

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,27 @@
-import javax.swing.*;
-import java.util.Random;
-
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 public class Main {
+   public Main() {
+   }
 
-    public static void main(String[] args) {
+   public static void main(String[] var0) {
+      Graph var1 = GraphData.createDeliveryNetwork();
+      if (var1 != null && var1.getVertexCount() >= 20) {
+         System.out.println("Graph initialized with " + var1.getVertexCount() + " delivery points.\n");
+         HamiltonianCycle var2 = new HamiltonianCycle(var1);
+         System.out.println("Searching for Hamiltonian cycle...\n");
+         int[] var3 = var2.findCycle();
+         if (var3 != null) {
+            if (var2.validateCycle(var3)) {
+               Printer.printCycle(var3);
+            } else {
+               Printer.printError("Invalid cycle");
+            }
+         } else {
+            Printer.printNoSolution();
+         }
 
-        Graph graph = GraphData.createDeliveryNetwork();
-        graph.generateRandomCoordinates(500, 500, new Random());
-
-        JFrame frame = new JFrame("Hamiltonian Cycle");
-        frame.setSize(600, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        GraphPanel panel = new GraphPanel(graph, null);
-        frame.add(panel);
-        frame.setVisible(true);
-
-        new Thread(() -> {
-
-            HamiltonianCycle solver = new HamiltonianCycle(graph);
-            GraphPanelCallback cb = new GraphPanelCallback(panel);
-
-            int[] cycle = solver.findCycleWithVisualization(cb);
-
-            if (cycle != null)
-                Printer.printCycle(cycle);
-            else
-                Printer.printNoSolution();
-
-        }).start();
-    }
+      } else {
+         Printer.printError("Invalid graph");
+      }
+   }
 }
